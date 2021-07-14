0e9a611e2132057e205ff57bca0eebd1517a33af
Boris Krasko <boris.krasko@icloud.com>
2021-07-15 01:51:19 +0300

    feat: add Dakwerken, Gevels, Renovatie

---
+++ js/gevels.js
@@ -0,0 +1,32 @@
+let variants = {
+  "gevels" : [
+    ["images/gevels.jpg"], {
+    "nl" :
+    [
+      ["Gevels"],
+      ["Soms is uw woning aan een verjongingskuur toe. Met een gevelrenovatie bereikt u verbluffende resultaten <strong>GG Floors&Construct</strong> renoveert voor u de gevel van uw woning tot in de puntjes. Gaande van zandstralen van de gevel, de voegen vervangen en/of reinigen tot een volledige gevelreiniging. Daarnaast ook pleisterwerk bijwerken of vernieuwen, de gevel afwerken met schilderwerk, vochtwerende behandelingen aanbrengen, vervangen of aanpassen van gevelmetselwerk. Uw woning krijgt meteen een heel andere uitstraling na zo’n gevelrenovatie. Naast gevelrenovaties kunnen wij ook metselwerken aanbieden."],
+    ],
+    "en" :
+    [
+      ["Facades"],
+      ["Sometimes your home needs a rejuvenation cure. You achieve amazing results with a facade renovation <strong>GG Floors&Construct</strong> renovates the facade of your home down to the last detail. Ranging from sandblasting the facade, replacing and/or cleaning the joints to a complete facade cleaning. In addition, also updating or renewing plaster, finishing the facade with paint, applying moisture-resistant treatments, replacing or adjusting facade masonry. Your home will immediately have a completely different look after such a facade renovation. In addition to facade renovations, we can also offer masonry works."],
+    ],
+     "de" :
+    [
+      ["Fassaden"],
+      ["Manchmal braucht Ihr Zuhause eine Verjüngungskur. Mit einer Fassadensanierung erzielen Sie erstaunliche Ergebnisse <strong>GG Floors&Construct</strong> saniert die Fassade Ihres Hauses bis ins Detail. Angefangen vom Sandstrahlen der Fassade über das Ersetzen und/oder Reinigen der Fugen bis hin zur kompletten Fassadenreinigung. Darüber hinaus aktualisieren oder erneuern wir auch Putze, veredeln die Fassade mit Farbe, führen feuchtigkeitsbeständige Behandlungen durch, ersetzen oder passen Fassadenmauerwerk an. Ihr Zuhause wird nach einer solchen Fassadensanierung sofort ein ganz anderes Aussehen haben. Neben Fassadensanierungen können wir auch Maurerarbeiten anbieten."],
+    ],
+     "fr" :
+    [
+      ["Façades"],
+      ["Parfois, votre maison a besoin d'une cure de rajeunissement. Vous obtenez des résultats étonnants avec une rénovation de façade <strong>GG Floors&Construct</strong> rénove la façade de votre maison dans les moindres détails. Cela va du sablage de la façade, du remplacement et/ou du nettoyage des joints au nettoyage complet de la façade. De plus, nous mettons également à jour ou renouvelons le plâtre, finissons la façade avec de la peinture, appliquons des traitements résistant à l'humidité, remplaçons ou ajustons la maçonnerie de façade. Votre maison aura immédiatement un aspect complètement différent après une telle rénovation de façade. En plus des rénovations de façades, nous pouvons également vous proposer des travaux de maçonnerie."],
+    ],
+  },
+  [
+    ["images/gallery/gevels/img-sm-1.jpg"],
+    ["images/gallery/gevels/img-sm-2.jpg"],
+    ["images/gallery/gevels/img-sm-3.jpg"],
+    ["images/gallery/gevels/img-sm-4.jpg"],
+  ]
+  ],
+}
