3ad47e2bec0b42ce35bd0d5bf8937cf2cf345b45
Boris Krasko <boris.krasko@icloud.com>
2021-07-15 00:38:42 +0300

    feat: add Terrassen en opritten

---
+++ js/terrassen-en-opritten.js
@@ -0,0 +1,33 @@
+let variants = {
+  "terrassen-en-opritten" : [
+    ["images/terrassen-en-opritten.jpg"], {
+    "nl" :
+    [
+      ["Terrassen en opritten"],
+      ["<strong>GG Floors&Construct</strong> verzorgt alle klinkerwerken en tegelwerken, van terrassen tot tuinpaden en van opritten tot trappen en parkings. Dit kan in natuursteen, kasseien, kleiklinkers, betonklinkers... Voor zowel grote als kleinere projecten kan je bij GG Floors&Construct terecht. Nauwkeurigheid en precizie zijn van het grootste belang voor GG Floors&Construct en zijn team. Bezit je een volledig uitgewerkt plan of wil je graag samen bekijken wat de mogelijkheden zijn, dit is allemaal perfect mogelijk. GG Floors&Construct en zijn team zijn over het hele land actief."],
+    ],
+    "en" :
+    [
+      ["Terraces and Driveways"],
+      ["<strong>GG Floors&Construct</strong> provides all clinker and tile work, from terraces to garden paths and from driveways to stairs and car parks. This is possible in natural stone, cobblestones, clay pavers, concrete pavers... You can contact GG Floors&Construct for both large and smaller projects. Accuracy and precision are of the utmost importance to GG Floors&Construct and his team. Do you have a fully elaborated plan or would you like to look at the possibilities together, this is all perfectly possible. GG Floors&Construct and his team are active all over the country."],
+    ],
+     "de" :
+    [
+      ["Terrassen und Einfahrten"],
+      ["<strong>GG Floors&Construct</strong> bietet alle Klinker- und Fliesenarbeiten, von Terrassen bis zu Gartenwegen und von Einfahrten bis hin zu Treppen und Parkplätzen. Dies ist bei Naturstein, Kopfsteinpflaster, Pflasterklinkern, Betonpflastersteinen möglich... Sie können GG Floors&Construct sowohl für große als auch für kleinere Projekte kontaktieren. Genauigkeit und Präzision sind für GG Floors&Construct und sein Team von größter Bedeutung. Haben Sie einen vollständig ausgearbeiteten Plan oder möchten Sie gemeinsam sehen, was die Möglichkeiten sind, ist dies alles bestens möglich. GG Floors&Construct und sein Team sind im ganzen Land aktiv."],
+    ],
+     "fr" :
+    [
+      ["Terrasses et allées"],
+      ["<strong>GG Floors&Construct</strong> fournit tous les travaux de clinker et de carrelage, des terrasses aux allées de jardin et des allées aux escaliers et parkings. C'est possible dans la pierre naturelle, les pavés, les pavés en terre cuite, les pavés en béton... Vous pouvez contacter GG Floors&Construct pour les grands et les petits projets. L'exactitude et la précision sont de la plus haute importance pour GG Floors&Construct et son équipe. Avez-vous un plan entièrement élaboré ou souhaitez-vous voir quelles sont les possibilités ensemble, tout cela est parfaitement possible. GG Floors&Construct et son équipe sont actifs dans tout le pays."],
+    ],
+  },
+  [
+    ["images/gallery/terrassen-en-opritten/img-sm-1.jpg"],
+    ["images/gallery/terrassen-en-opritten/img-sm-2.jpg"],
+    ["images/gallery/terrassen-en-opritten/img-sm-3.jpg"],
+    ["images/gallery/terrassen-en-opritten/img-sm-4.jpg"],
+    ["images/gallery/terrassen-en-opritten/img-sm-5.jpg"],
+  ]
+  ],
+}
