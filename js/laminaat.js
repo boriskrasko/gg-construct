7bc6067c687f7a841e4d4e88459e80dc7e9842b3
Boris Krasko <boris.krasko@icloud.com>
2021-07-20 01:40:39 +0300

    feat: add Laminaat

---
+++ js/laminaat.js
@@ -0,0 +1,29 @@
+let variants = {
+  "laminaat" : [
+    ["images/laminaat.jpg"], {
+    "nl" :
+    [
+      ["Laminaat"],
+      ["Laminaat. Het uitzicht van laminaat wordt bepaald door de tekening die als decorlaag gebruikt wordt. Toch is het oppervlak van laminaat bijna niet te onderscheiden van echt parket. Bovendien is laminaat onderhoudsvriendelijker dan parket en kan je laminaat makkelijk zelf plaatsen dankzij het handige kliksysteem, ook in combinatie met vloerverwarming."],
+    ],
+    "en" :
+    [
+      ["Laminate flooring"],
+      ["Laminate. The appearance of laminate is determined by the drawing that is used as a decorative layer. Yet the surface of laminate is almost indistinguishable from real parquet. Laminate is also easier to maintain than parquet and you can easily install laminate yourself thanks to the handy click system , also in combination with underfloor heating."],
+     ],
+    "de" :
+    [
+      ["Laminatboden"],
+      ["Laminat. Die Optik von Laminat wird durch die Zeichnung bestimmt, die als Dekorschicht verwendet wird. Dabei ist die Oberfläche von Laminat kaum von echtem Parkett zu unterscheiden. Laminat ist zudem pflegeleichter als Parkett und Sie können Laminat dank das handliche Klicksystem , auch in Kombination mit Fußbodenheizung."],
+     ],
+    "fr" :
+    [
+      ["Revêtement de sol stratifié"],
+      ["Stratifié. L'aspect du stratifié est déterminé par le dessin qui est utilisé comme couche décorative. Pourtant, la surface du stratifié est presque indiscernable du vrai parquet. Le stratifié est également plus facile à entretenir que le parquet et vous pouvez facilement installer vous-même le stratifié grâce à le système Click pratique, également en combinaison avec le chauffage par le sol."],
+     ],
+    },
+  [
+    
+  ]
+  ],
+}
