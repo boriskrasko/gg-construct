9e373048354e9ba512b1e21609f89749f1ba69c5
Boris Krasko <boris.krasko@icloud.com>
2021-07-30 00:31:15 +0300

    feat: add LVT

---
+++ js/lvt.js
@@ -0,0 +1,34 @@
+let variants = {
+  "lvt" : [
+    ["images/lvt.jpg"], {
+    "nl" :
+    [
+      ["LVT"],
+      ["Zijn een ongelooflijk populaire vloeroptie in zowel huishoudelijke als commerciële gebouwen. Het is bekend voor het imiteren van hout en tegels met onberispelijke resultaten. Terwijl LVT op zichzelf een ding van schoonheid is, kiezen veel mensen ervoor om hun uiterlijk te verbeteren met behulp van featurestroken. We hebben deze complete gids samengesteld om u te helpen het indrukwekkende verschil te zien dat LVT-functiestroken kunnen maken voor uw gloednieuwe vloer!"]
+    ],
+    "en" :
+    [
+      ["LVT"],
+      ["Its an incredibly popular flooring option in both residential and commercial buildings. It is known for imitating wood and tile with impeccable results. While LVT is a thing of beauty in its own right, many people choose to enhance their appearance using We've put together this complete guide to help you see the impressive difference LVT feature strips can make to your brand new floor!"]
+    ],
+    "de" :
+    [
+      ["LVT"],
+      ["Es ist eine unglaublich beliebte Bodenbelagsoption in Wohn- und Geschäftsgebäuden. Er ist dafür bekannt, Holz und Fliesen mit tadellosen Ergebnissen zu imitieren. Obwohl LVT an sich schon eine Schönheit ist, entscheiden sich viele Menschen dafür, ihr Erscheinungsbild mit We've zu verbessern stellen Sie diesen vollständigen Leitfaden zusammen, um Ihnen zu zeigen, welchen beeindruckenden Unterschied LVT-Funktionsstreifen für Ihren brandneuen Boden machen können!"]
+    ],
+    "fr" :
+    [
+      ["LVT"],
+      ["C'est une option de revêtement de sol incroyablement populaire dans les bâtiments résidentiels et commerciaux. Il est connu pour imiter le bois et le carrelage avec des résultats impeccables. Bien que le LVT soit une beauté en soi, de nombreuses personnes choisissent d'améliorer leur apparence en utilisant We've rassemblez ce guide complet pour vous aider à voir la différence impressionnante que les bandes de caractéristiques LVT peuvent apporter à votre tout nouveau sol !"]
+    ],
+  },
+  [
+  //   ["images/gallery/lvt/img-sm-1.jpg"],
+  //   ["images/gallery/lvt/img-sm-2.jpg"],
+  //   ["images/gallery/lvt/img-sm-3.jpg"],
+  //   ["images/gallery/lvt/img-sm-4.jpg"],
+  //   ["images/gallery/lvt/img-sm-5.jpg"],
+  //   ["images/gallery/lvt/img-sm-6.jpg"],
+  ]
+  ],
+}
