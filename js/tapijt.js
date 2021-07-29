6ce478b2f6fe8e1f0644ddbc2cd1e0b156ca711d
Boris Krasko <boris.krasko@icloud.com>
2021-07-30 00:03:55 +0300

    feat: add Tapijt

---
+++ js/tapijt.js
@@ -0,0 +1,39 @@
+let variants = {
+  "tapijt" : [
+    ["images/tapijt.jpg"], {
+    "nl" :
+    [
+      ["Tapijt"],
+      ["Tapijten komen in vele vormen voor. Tapijt is verkrijgbaar in rollen, stroken en tegelvormen. Ook wordt er vaak gesproken over perzische of oosterse tapijten, maar wat wil dat nu allemaal zeggen? Wij gingen op zoek naar antwoorden. Er zijn verschillende soorten tapijten, afhankelijk van de gebruikte productietechnieken of structuur van het tapijt. De drie meest gebruikte technieken bij tapijt zijn: Tuften, weven en knopen. De structuur van een tapijt bestaat meestal uit drie delen: de rug (onderste deel), daarop het tuft- of draagdoek en daarop de pool Tappijttegels hebben veel van de voordelen van de gewone tapijten: ze zijn comfortabel, voelen warm en zacht aan, ze zijn tevens geluidsdempend en warmte-isolerend en de kans op slippen is ook minimaal. Bijkomende voordelen zijn dan ook dat een aparte tegel kan vervangen worden, maar ook dat je zelf creativiteit kan inbrengen bij de legging van tapijttegels."]
+    ],
+    "en" :
+    [
+      ["Carpet"],
+      ["Carpets come in many forms. Carpet is available in rolls, strips and tile shapes. It is also often referred to as Persian or Oriental carpets, but what does that all mean? We went looking for answers. There are different types of carpets. , depending on the production techniques or structure of the carpet used.The three most commonly used techniques in carpet are: Tufting, weaving and knotting.The structure of a carpet usually consists of three parts: the back (bottom part), then the tuft or sling and the pile Carpet tiles have many of the advantages of regular carpets: they are comfortable, feel warm and soft, they are also sound dampening and heat insulating and the chance of slipping is also minimal. separate tile can be replaced, but also that you can bring in your own creativity when laying carpet tiles."]
+    ],
+    "de" :
+    [
+      ["Teppich"],
+      ["Teppiche gibt es in vielen Formen. Teppiche gibt es in Rollen-, Streifen- und Fliesenformen. Auch Perser- oder Orientteppiche werden oft genannt, aber was bedeutet das alles? Wir haben uns auf die Suche nach Antworten gemacht. Es gibt verschiedene Arten von Teppichen auf die Herstellungstechniken oder die Struktur des verwendeten Teppichs. Die drei am häufigsten verwendeten Techniken bei Teppichen sind: Tuften, Weben und Knüpfen Tuft oder Schlinge und der Flor Teppichfliesen haben viele Vorteile herkömmlicher Teppiche: Sie sind bequem, fühlen sich warm und weich an, sind zudem schall- und wärmedämmend und die Rutschgefahr ist ebenfalls minimal auch, dass Sie beim Verlegen von Teppichfliesen Ihre eigene Kreativität einbringen können."]
+    ],
+    "fr" :
+    [
+      ["Tapis"],
+      ["Les tapis se présentent sous de nombreuses formes. Les tapis sont disponibles en rouleaux, en bandes et en dalles. Les tapis persans ou orientaux sont également souvent mentionnés, mais qu'est-ce que tout cela signifie ? Nous sommes allés chercher des réponses. Il existe différents types de tapis. sur les techniques de production ou la structure du tapis utilisé. Les trois techniques les plus couramment utilisées en tapis sont : le tuftage, le tissage et le nouage. La structure d'un tapis se compose généralement de trois parties : le dos (partie inférieure), au-dessus duquel le touffe ou élingue et le velours Les dalles de moquette présentent de nombreux avantages des moquettes ordinaires : elles sont confortables, chaudes et douces, elles sont également insonorisantes et isolantes thermiquement et le risque de glisser est également minime. Des dalles séparées peuvent être remplacées, mais aussi que vous pouvez apporter votre propre créativité lors de la pose de dalles de moquette."]
+    ],
+  },
+  [
+    ["images/gallery/tapijt/img-sm-1.jpg"],
+    ["images/gallery/tapijt/img-sm-2.jpg"],
+    ["images/gallery/tapijt/img-sm-3.jpg"],
+    ["images/gallery/tapijt/img-sm-4.jpg"],
+    ["images/gallery/tapijt/img-sm-5.jpg"],
+    ["images/gallery/tapijt/img-sm-6.jpg"],
+    ["images/gallery/tapijt/img-sm-7.jpg"],
+    ["images/gallery/tapijt/img-sm-8.jpg"],
+
+
+
+  ]
+  ],
+}
